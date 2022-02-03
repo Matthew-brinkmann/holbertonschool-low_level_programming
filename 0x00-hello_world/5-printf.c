5a90e549aeef2a8cc019624e80a8209eaea07717
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-02 17:10:27 -0800

    upload step 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/* adding code to this file */
+#include <stdio.h>
+
+/**
+ * main - this funciton prints the string to the screen
+ *
+ * Description: just as stated, prints the string to the screen
+ *
+ * Return: 0 at the end of the function
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
