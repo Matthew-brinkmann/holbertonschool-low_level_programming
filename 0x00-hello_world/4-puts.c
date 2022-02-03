117c14d5fb90adc19b2f5572f5c4d80dbf98d0f3
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-02 17:06:44 -0800

    fixed betty style errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 /* adding code to this file */
 #include <stdio.h>
 
+/**
+ * main - this funciton prints the string to the screen
+ *
+ * Description: just as stated, prints the string to the screen
+ *
+ * Return: 0 at the end of the function
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
