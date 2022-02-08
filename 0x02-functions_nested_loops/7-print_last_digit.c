359782123a3265f6a3b36fbc9874445b3a6cb441
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-08 00:20:14 -0800

    adding step 7 and updating header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of the integer
+ * passed through the functio
+ * @n: number we want to find the last digit of
+ * Description: long description
+ *
+ * Return: jthe last number of the int entered
+ */
+int print_last_digit(int n)
+{
+	int x = n % 10;
+	if (x < 0)
+	{
+		x = x * -1;
+	}
+	_putchar('0' + x);
+	return (x);
+}
