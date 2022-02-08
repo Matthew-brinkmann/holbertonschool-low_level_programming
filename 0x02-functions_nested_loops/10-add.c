5228cb2c1d596c177ed837fc07b1d7d15afa9ae2
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-08 01:41:33 -0800

    adding step 10 and update header

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers together
+ * @a: first interger to add
+ * @b: second integer to add
+ *
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+int add(int a, int b)
+{
+	int x;
+
+	x = a + b;
+
+	return (x);
+}
