c02c181256463b2d99b52c695979dcd8f00cbb2b
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-08 04:17:02 -0800

    adding advanced task 1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,37 +14,41 @@ void print_times_table(int n)
 	/* x = total number*/
 	int x;
 
-	for (b = 0; b <= n; b++)
+	if (n > 0 && n < 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (b = 0; b <= n; b++)
 		{
-			x = a * b;
-			if (a == 0)
+			for (a = 0; a <= n; a++)
 			{
-				_putchar((x) + '0');
-			} else if (x < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((x) + '0');
-			} else if (x > 10 && x < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-			} else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((x / 100) + '0');
-				_putchar(((x / 10) % 10) + '0');
-				_putchar(((x) % 10) + '0');
+				x = a * b;
+				if (a == 0)
+				{
+					_putchar((x) + '0');
+				} else if (x < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((x) + '0');
+				} else if (x > 9 && x < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((x / 100) + '0');
+					_putchar(((x / 10) % 10) + '0');
+					_putchar(((x) % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
+	} else
 		_putchar('\n');
-	}
 }
