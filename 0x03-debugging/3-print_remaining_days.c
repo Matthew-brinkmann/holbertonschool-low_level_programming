322f0ccaad633dd7d63f0ccd79d8438fe309dfa6
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-09 01:35:08 -0800

    chaged if statement

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,13 +16,13 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (!(year % 100 == 0))
 		{
-			if (month >= 2 && day >= 60)
+			if (month >= 2)
 			{
 				day++;
 			}
 		} else if (year % 100 == 0 && year % 400 == 0)
 		{
-			if (month >= 2 && day >= 60)
+			if (month >= 2)
 			{
 				day++;
 			}
