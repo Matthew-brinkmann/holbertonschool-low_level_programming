ae3c66ed88559e90c8677019dfae5d91b75428da
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-09 14:21:55 -0800

    adding step 6 and updating header file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - will print a line as long as n
+ * @n: the length of the line that needs printing
+ *
+ * Description: long description
+ *
+ * Return: nothing
+ */
+void print_line(int n)
+{
+	int x = 0;
+
+	if (n > 0)
+	{
+		while (x <= n)
+		{
+			_putchar('_');
+			x++;
+		}
+	}
+	_putchar('\n');
+}
