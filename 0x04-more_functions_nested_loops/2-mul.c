3cb4575fc1a3a36d446ef4c6e82fa97e203030d2
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-09 12:58:09 -0800

    addiing step 2 and updating header file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - will multiply 2 digits
+ * @a: first digit entered
+ * @b: second figit entered
+ * Description: long description
+ *
+ * Return: the multiplication of int a and int b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
