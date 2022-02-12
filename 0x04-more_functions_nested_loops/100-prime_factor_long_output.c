6e5ff86a6883f982a8486f263a6ab84801d63816
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-12 15:31:51 -0800

    made code neater

--- 0x04-more_functions_nested_loops/100-prime_factor_long_output.c
+++ 0x04-more_functions_nested_loops/100-prime_factor_long_output.c
@@ -16,20 +16,20 @@ long lastTested = 2;
  */
 void find_prime(long n)
 {
-        long i,m=0,flag=0;
+        long i ,m = 0, flag = 0;
 
-	m=n/2;
-	for(i=2;i<=m;i++)
+	m = n / 2;
+	for (i = 2; i <= m; i++)
 	{
-		if(n%i==0)
+		if (n % i == 0)
 		{
 			printf("%li is not prime\n", n);
-			flag=1;
+			flag = 1;
 			lastTested = n;
 			break;
 		}
 	}
-	if(flag==0)
+	if (flag == 0)
 	{
 		printf("%li is prime\n", n);
 		if (n > highest)
