c44bc50b4841df6a245f4b0072563aeb0adcf978
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-13 15:10:39 -0800

    pushing all base files and task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - will reset value n to 98
+ * @n: the address we are resetting to 98.
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+# Readme for Pointers project
+## by Matthew Brinkmann
+
+No need for much more just yet
