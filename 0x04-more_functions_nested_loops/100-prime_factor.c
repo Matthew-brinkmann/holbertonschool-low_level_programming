51dc61fbdcdae3e1b8a8ac7d98033bc154d8027e
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-12 17:16:25 -0800

    adding betty compliant code

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,54 +1,71 @@
 #include <stdio.h>
-long highest = 0;
-long lastTested = 2;
 
-void find_prime(long n)
+/**
+ * find_prime - will find if the number entered is a prime number or not
+ * @n: the inputted number
+ *
+ * Description: This is a bit inefficient however it works, this function
+ * just checks the number against every number of half its value
+ * to see if the modulos (modolus)? is 0? each prime number is written
+ * to the global variable highest, if it is bigger than the variable
+ * already stored
+ *
+ * Return: will not return anything.
+ */
+long find_prime(long n)
 {
-        long i,m=0,flag=0;
+	long i, m = 0, flag = 0;
 
-	m=n/2;
-	for(i=2;i<=m;i++)
+	m = n / 2;
+	for (i = 2; i <= m; i++)
 	{
-		if(n%i==0)
+		if (n % i == 0)
 		{
-			printf("%li is not prime\n", n);
-			flag=1;
-			lastTested = n;
-			break;
+			flag = 1;
+			return (0);
 		}
 	}
-	if(flag==0)
+	if (flag == 0)
 	{
-		printf("%li is prime\n", n);
-		if (n > highest)
-			lastTested = n;
-			highest = n;
+		return (n);
 	}
+	return (0);
 }
 
-void find_factor(long n)
-{
-	long test = 1;
-
-	putchar('\n');
-	while (test < lastTested)
-	{
-		if (n % test == 0)
-		{
-			find_prime(test);
-			find_prime(n / test);
-			test = test + 1;
-		}
-		else
-			test = test + 1;
-	}
-}
-
-long main(void)
+/**
+ * main - will find the factor of the number selected
+ *
+ * Description: Numbers are passed through this functions, and if
+ * the number has factors (x * y = n) then the factors are passed to
+ * the find_prime function to see if it is a prime number
+ * each factor is tested in pairs, and the highest paired number
+ * is stored so it won't bother checking numbers it
+ * has already tested.
+ *
+ * Return: will return (0);
+ */
+int main(void)
 {
 	long x = 612852475143;
-	printf("starting code\n");
-	find_factor(x);
-	printf("%li is the highest prime number of %li\n", highest, x);
+	long lastTested = x;
+	long highest = 1;
+	long highestYet = 0;
+	long testInc = 1;
+
+	while (testInc < lastTested)
+	{
+		if (x % testInc == 0)
+		{
+			lastTested = x / testInc;
+			highestYet = find_prime(testInc);
+			if (highestYet > highest)
+				highest = highestYet;
+			highestYet = find_prime(x / testInc);
+			if (highestYet > highest)
+				highest = highestYet;
+		}
+		testInc = testInc + 1;
+	}
+	printf("%li\n", highest);
 	return (0);
 }
