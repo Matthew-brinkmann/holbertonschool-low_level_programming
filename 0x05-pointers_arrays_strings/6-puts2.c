68f7d3520c655eadb22ccc4b9c9daac5d33c2e95
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-13 20:49:57 -0800

    update task 6 code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * _strlen - counts length of string
+ * @s: string to count
+ * Description: long description
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	char string = *s;
+	int counter = 0;
+
+	while (string != '\0')
+	{
+		counter++;
+		string = *(s + counter);
+	}
+	return (counter);
+}
+/**
  * puts2 - prints every second line of a string
  * @str: string to display
  * Description: long description
@@ -9,10 +28,17 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int counter;
+	int i = 0;
+	counter = _strlen(str);
+	while (i < counter)
 	{
-		_putchar(*str);
-		str = str + 2;
+		while (*str != '\0')
+		{
+			_putchar(*str);
+			str = str + 2;
+		}
+		i++;
 	}
 	_putchar('\n');
 }
