64231b0729f60d0628b8015f57e9c1f973415006
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-13 19:12:59 -0800

    adding step 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - will return the length of the string passed through
+ * @s: the string we need to count
+ * Description: long description
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	char string = *s;
+	int counter = 0;
+
+	while (string != '\0')
+	{
+		counter++;
+		string = *(s + counter);
+	}
+	return (counter);
+}
