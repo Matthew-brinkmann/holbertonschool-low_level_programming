6322b47e6bdff95b6defc85f8719592742e47275
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-14 00:49:12 -0800

    adding step 7 and update header

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * _strlen - will return the length of the string passed through
+ * @s: the string we need to count
+ * Description: long description
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	char string = *s;
+	int counter = 0;
+
+	while (string != '\0')
+	{
+		counter++;
+		string = *(s + counter);
+	}
+	return (counter);
+}
+/**
+ * puts_half - short description
+ * @str: string we want to print half of.
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void puts_half(char *str)
+{
+	int counter = _strlen(str);
+	int i = 0;
+
+	while (i <= counter / 2)
+	{
+		i++;
+	}
+	while (i < counter)
+		{
+			_putchar(str[i]);
+			i++;
+		}
+	_putchar('\n');
+}
