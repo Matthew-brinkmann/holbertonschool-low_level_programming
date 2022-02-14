61d3e6c209c8c0cee3ee1046bd7ab9269e10b6d2
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-14 01:17:48 -0800

    adding task 8 and updated header

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - will print the array in order
+ * @a: the array where things are stored
+ * @n: the number of elements in the array to print.
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		printf("%d", a[i]);
+		if (i < n)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
