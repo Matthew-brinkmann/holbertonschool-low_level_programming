7685d273b842162528edb58678e4c02bc2aad4be
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-14 04:26:33 -0800

    checking agian

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
 
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand();
-	printf("%d\n", n);
-	return (n);
+	printf("%d", n);
 }
