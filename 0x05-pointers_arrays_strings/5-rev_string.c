4d737d525a5edd26886eadbaf3d4a255064b1e7e
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-13 20:37:27 -0800

    adding task 5, update header file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverse a string in the memory
+ * @s: string to reverse in memory
+ * Description: long description
+ *
+ * Return: won't return anything
+ */
+void rev_string(char *s)
+{
+	char string = *s;
+	char newString;
+	int counter = 0;
+	int i = 0;
+
+	printf("start of my function");
+	while (string != '\0')
+	{
+		counter++;
+		string = *(s + counter);
+	}
+	counter--;
+
+	while (i < counter)
+	{
+		newString = s[i];
+		s[i] = s[counter];
+		s[counter] = newString;
+		counter--;
+		i++;
+	}
+	printf("end of my function");
+}
