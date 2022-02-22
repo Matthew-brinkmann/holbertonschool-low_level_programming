e3d98b18957fc7a0234f11d9b613c058d4f84178
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-02-22 14:18:29 -0800

    adding task 1 and update header

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - print a string in reverse
+ * @s: the character we are going to print
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);
+}
