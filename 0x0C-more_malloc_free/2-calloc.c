8c8631dbf3d4dddee36214afe37e5651ab62b31c
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-02 13:43:53 -0800

    adding task 2 and update header

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Write a function that allocates memory for an array
+ * @nmemb: the number of elements in the array
+ * @size: the size of each array element
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int counter = 0;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+
+	while (counter < size)
+	{
+		array[counter] = 0;
+		counter++;
+	}
+
+	return (array);
+}
