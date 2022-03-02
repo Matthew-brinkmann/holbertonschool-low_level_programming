48ff1e0b4ceeb2d386f600633733030a880638e4
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-01 18:38:23 -0800

    making code cleaner, changed s1 and s2 declarations

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,32 +22,6 @@ int _strlen(char *s)
 }
 
 /**
- * _strncat - add src
- * to the end of dest until n.
- * @dest: will be the string that is added too
- * @src: the string to copy
- * @n: the amount of src to copy
- * Description: long description
- *
- * Return: pointer to dest.
- */
-char *_strncat(char *dest, char *src, int n)
-{
-
-	int startCopy = _strlen(dest);
-	int i = 0;
-
-	while (src[i] != '\0' && i < n)
-	{
-		dest[startCopy] = src[i];
-		i++;
-		startCopy++;
-	}
-	dest[startCopy + 1] = '\0';
-	return (dest);
-}
-
-/**
  * _strncpy - a string to copy to a saved buffer
  * @dest: buffer for string
  * @src: the string we are going to copy
@@ -87,12 +61,18 @@ char *str_concat(char *s1, char *s2)
 	char *Con;
 
 	if (s1 == NULL)
+	{
 		lenS1 = 0;
+		s1 = "";
+	}
 	else
 		lenS1 = _strlen(s1);
 
 	if (s2 == NULL)
+	{
 		lenS2 = 0;
+		s2 = "";
+	}
 	else
 		lenS2 = _strlen(s2);
 
@@ -100,15 +80,8 @@ char *str_concat(char *s1, char *s2)
 	if (Con == NULL)
 		return (0);
 
-	if (s1 != NULL && s2 != NULL)
-	{
-		_strncpy(Con, s1, lenS1);
-		_strncat(Con, s2, lenS2);
-	}
-	else if (s1 != NULL && s2 == NULL)
-		_strncpy(Con, s1, lenS1 + 1);
-	else if (s1 == NULL && s2 != NULL)
-		_strncpy(Con, s2, lenS2 + 1);
+	_strncpy(Con, s1, lenS1);
+	_strncpy(Con + lenS1, s2, lenS2);
 
 	return (Con);
 }
