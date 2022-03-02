35023aef38079427cf892faac8116e90e760b99a
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-02 13:18:10 -0800

    initial repo setup and task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Write a function that allocates memory using malloc
+ * @b: size of memory allocation
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *src = malloc(sizeof *src * b);
+
+	if (src == NULL)
+		exit(98);
+	return (src);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+# Readme for malloc more project
+## By Matthew Brinkmann
+
+Will add more if needed
