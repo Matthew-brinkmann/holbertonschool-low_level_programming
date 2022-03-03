91f9297174b0dbe348d5db608fc6a9ecbfb32731
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-03 14:43:01 -0800

    update task to treat null value

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -53,7 +53,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	if (ptr == NULL)
+	{
+		p = malloc(new_size)
+			if (new == NULL)
+				return (NULL);
 		return (p);
+	}
 	write = p;
 	copy = ptr;
 
