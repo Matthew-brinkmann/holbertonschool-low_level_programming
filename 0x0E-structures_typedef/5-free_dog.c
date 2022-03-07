5e934b44c1bbe487cf00fb5d3fa8650dded90cd4
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-06 17:52:23 -0800

    adding task 5 and update dog header

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - will free memory oof newly created dog
+ * @d: pointer to newly created dog
+ * Description: long description
+ *
+ * Return: pointer to new dog information
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
