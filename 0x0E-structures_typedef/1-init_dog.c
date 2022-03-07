764f739681b7b1a2c0df114bd88d4cd3d579d410
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-06 17:15:43 -0800

    update header and upload step 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to start of structure
+ * @name: name of new dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
