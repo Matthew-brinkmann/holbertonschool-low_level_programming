1083fd50052adef4be63e8e731561058819c9550
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-06 16:35:13 -0800

    adding task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - just prints the name of file
+ *
+ * Description: long description
+ *
+ * Return: returns 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
