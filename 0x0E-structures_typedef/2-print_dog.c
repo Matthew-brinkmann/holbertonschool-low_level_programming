32fa50171ee5a616d7d77321c69c53d7a07e8ee3
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-06 17:31:19 -0800

    adding task 2 and updating dog header

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - will print info stored in dog
+ * @d: pointer to dog structure.
+ * Description: long description
+ *
+ * Return: add in what is supposed to be returned if anything
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (d->age == 0)
+			printf("Age: (nil)");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
