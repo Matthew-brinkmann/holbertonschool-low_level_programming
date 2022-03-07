7f00adb60971e8e1d0ca1ecdbc21bc35d6af85d5
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-06 19:30:26 -0800

    update task 4 to change memcopy to strdup

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,27 +29,51 @@ int _strlen(char *s)
 }
 
 /**
- * _memcpy - short description
- * @src: the memory source we want to copy
- * @dest: the location we want to store the memory
- * @n: the number of bytes to copy.
+ * _strncpy - a string to copy to a saved buffer
+ * @dest: buffer for string
+ * @src: the string we are going to copy
+ * @n: the number of characters to copy
  * Description: long description
  *
- * Return: returns pointer to dest
+ * Return: the pointer to dest
  */
-void *_memcpy(void *dest, void *src, unsigned int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	unsigned int counter = 0;
-	char *fillDest = dest;
-	char *fillSrc = src;
+	int i = 0;
 
-	while (counter < n)
+	while ((i < n) && (src[i] != '\0'))
 	{
-		fillDest[counter] = fillSrc[counter];
-		counter++;
+		dest[i] = src[i];
+		i++;
 	}
-
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
+}
+
+/**
+ * _strdup - duplicate string
+ * @str: the string we will duplicate.
+ * Description: long description
+ *
+ * Return: pointer to new string or null
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *cpy;
+
+	if (str == NULL)
+		return (0);
+	len = _strlen(str);
+	cpy = malloc(len + 1);
+	if (cpy == NULL)
+		return (0);
+	_strncpy(cpy, str, len);
+	return (cpy);
 }
 
 /**
@@ -64,37 +88,20 @@ void *_memcpy(void *dest, void *src, unsigned int n)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog = malloc(sizeof(dog_t));
-	char *storeName;
-	char *storeOwner;
 
 	if (newdog == NULL)
 		return (NULL);
 
-	if (_strlen(name) > 0)
-	{
-		storeName = malloc(_strlen(name) + 1);
-		if (storeName != NULL)
-		{
-			_memcpy(storeName, name, _strlen(name) + 1);
-			newdog->name = storeName;
-		}
-	}
-	else
-		newdog->name = name;
-
-	if (_strlen(owner) > 0)
-	{
-		storeOwner = malloc(_strlen(owner) + 1);
-		if (storeOwner != NULL)
-		{
-			_memcpy(storeOwner, owner, _strlen(owner) + 1);
-			newdog->owner = storeOwner;
-		}
-	}
-	else
-		newdog->owner = owner;
-
+	newdog->name = _strdup(name);
 	newdog->age = age;
+	newdog->owner = _strdup(owner);
 
+	if (newdog->name == 0 || newdog->owner == 0)
+	{
+		free(newdog->name);
+		free(newdog->owner);
+		free(newdog);
+		return (NULL);
+	}
 	return (newdog);
 }
