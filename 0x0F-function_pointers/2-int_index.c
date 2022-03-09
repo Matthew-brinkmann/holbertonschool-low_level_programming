af6dd81d3b9e1ae8acb6b0fce46eb98e0a5fafff
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-08 17:08:08 -0800

    fixing handle of NULL pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,12 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	while (i < size)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
-		i++;
+		while (i < size)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+			i++;
+		}
 	}
-
 	return (-1);
 }
