6d9c4d511253f11c9c40fb5dd3ba90baa6d987cc
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-16 16:36:53 -0700

    adding task 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - add an elemt to the start of the list.
+ * @head: the list to display
+ * @str: the string to add to the list
+ * Description: long description
+ *
+ * Return: address of new item.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *strCpy;
+	int strLen = 0;
+	list_t *new = NULL;
+	list_t *hold = NULL;
+
+	hold = *head;
+	if (hold != NULL)
+	{
+		while (hold->next != NULL)
+			hold = hold->next;
+	}
+	strCpy = strdup(str);
+	if (strCpy == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	while (strCpy[strLen] != '\0')
+	{
+		strLen++;
+	}
+
+	new->str = strCpy;
+	new->len = strLen;
+	new->next = NULL;
+	if (hold != NULL)
+		hold->next = new;
+	else
+		*head = new;
+	return (new);
+}
