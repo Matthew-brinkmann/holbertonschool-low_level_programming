196d5f80ea336c126794b63e48fb9d7b66e8eb72
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-16 18:36:17 -0700

    adding advanced tasl

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * myStartupFun - prints a sentence before main is executed
+ *
+ * Description: long description
+ *
+ * Return: nothing.
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
