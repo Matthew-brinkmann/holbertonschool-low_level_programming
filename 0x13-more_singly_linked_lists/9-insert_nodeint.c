87578fdd037c821defa76c4b0d0a23c682b45a87
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-21 19:22:19 -0700

    re-ordered code to remove an else statement (to remove choices for PC), found a missing nodeCount++; not sure how code was working as well as it was, but included missing line

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,24 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL && idx > 0)
 		return (NULL);
 	hold = *head;
-	if (idx != 0)
-	{
-		while (hold != NULL)
-		{
-			if (nodeCount + 1 == idx)
-			{
-				new = malloc(sizeof(*new));
-				if (new == NULL)
-					return (NULL);
-				new->n = n;
-				new->next = hold->next;
-				hold->next = new;
-				return (new);
-			}
-			hold = hold->next;
-		}
-	}
-	else
+	if (idx == 0)
 	{
 		new = malloc(sizeof(*new));
 		if (new == NULL)
@@ -48,6 +31,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = hold;
 		*head = new;
 		return (new);
+
+	}
+	while (hold != NULL)
+	{
+		if (nodeCount + 1 == idx)
+		{
+			new = malloc(sizeof(*new));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = hold->next;
+			hold->next = new;
+			return (new);
+		}
+		hold = hold->next;
+		nodeCount++;
 	}
 
 	return (NULL);
