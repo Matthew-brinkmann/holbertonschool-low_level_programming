5bbd59f3d86d1015fcbfa140839f854be933a891
Matthew-brinkmann <matthew.brinkmann@hotmail.com>
2022-03-22 04:34:49 -0700

    return value changed

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,55 @@
 #include "lists.h"
 
 /**
+ * node_count - will calculate the distance of nodes between 1st and current.
+ * @head: head of the listInt
+ * @current: Current list of lintInt
+ * Return: the number of nodes in list or exit on 98 on error.
+ */
+int node_count(const listint_t *head, const listint_t *current)
+{
+	int counter = 0;
+	const listint_t *move;
+
+	move = head;
+	if (move == current)
+		return (1);
+
+	while (move != current)
+	{
+		counter++;
+		move = move->next;
+	}
+
+	return (counter + 1);
+}
+
+/**
+ * print_listint_n - prints list type listint to element n.
+ * @h: the list to print
+ * @index: the number of nodes to print.
+ * Description: long description
+ *
+ * Return: the number of elements in the list.
+ */
+int print_listint_n(const listint_t *h, int index)
+{
+	int nodeCount = 0;
+
+	if (h == NULL)
+		return (nodeCount);
+
+	while (nodeCount < index)
+	{
+		printf("[%p] %d\n", (void *)h, h->n);
+		h = h->next;
+		nodeCount++;
+	}
+
+	return (nodeCount);
+}
+
+/**
  * print_listint_safe - print a list that doesn't print looped elements
  * @head: the list to print
  *
@@ -10,26 +59,37 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
+	size_t retVal = 0;
+	int TotalCount = -1, newCount = 0;
 	const listint_t *hold = NULL, *loop = NULL;
 
-	hold = head;
 	if (head == NULL)
-		exit(98);
+		return (retVal);
 
-	while (hold != NULL)
+	hold = head;
+	loop = head;
+
+	while (newCount > TotalCount && loop != NULL)
 	{
-		loop = hold->next;
-		printf("[%p] %d\n", (void *)hold, hold->n);
-		if (loop > hold)
-		{
-			printf("-> [%p] %d\n", (void *)loop, loop->n);
+		TotalCount = newCount;
+		newCount = node_count(hold, loop);
+		if (newCount < TotalCount)
 			break;
-		}
-		hold = hold->next;
-		count++;
+		loop = loop->next;
+
 	}
 
-
-	return (count);
+	if (loop == NULL)
+	{
+		retVal = newCount;
+		print_listint_n(head, newCount);
+		return (retVal);
+	}
+	else
+	{
+		retVal = TotalCount;
+		print_listint_n(head, TotalCount);
+		printf("-> [%p] %d\n", (void *)loop, loop->n);
+		return (retVal);
+	}
 }
