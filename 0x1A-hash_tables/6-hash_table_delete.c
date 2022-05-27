ebd5d681d23b8e8283776fca8d9f132a8a83ed99
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-05-26 17:50:00 -0700

    build: adding task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *next_node = NULL, *node = NULL;
+
+	if (!ht)
+		return;
+
+	if (ht->array)
+	{
+		for (; i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				node = ht->array[i];
+				while (node)
+				{
+					next_node = node->next;
+					if (node->value)
+						free(node->value);
+					if (node->key)
+						free(node->key);
+					free(node);
+					node = next_node;
+				}
+			}
+		}
+		free(ht->array);
+	}
+	free(ht);
+}
