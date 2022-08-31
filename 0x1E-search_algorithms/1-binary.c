1e588afb1654d7bdda48ab289dbfb8108f10b29b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-31 15:13:01 -0700

    fix: print array order

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -33,22 +33,20 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t low, mid, high;
 
-	if (array != NULL && size != 0)
+	if (array == NULL || size == 0 || array[0] > value)
+		return (-1);
+	low = 0;
+	high = size - 1;
+	while (low <= high)
 	{
-		low = 0;
-		high = size - 1;
 		array_print(array + low, high - low + 1);
-		while (low <= high)
-		{
-			mid = low + (high - low) / 2;
-			if (array[mid] == value)
-				return (mid);
-			if (array[mid] < value)
-				low = mid + 1;
-			else
-				high = mid - 1;
-			array_print(array + low, high - low + 1);
-		}
+		mid = low + (high - low) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
 	}
 	return (-1);
 }
